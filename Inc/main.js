c1cdda217d2a4960f773dc6c03a78bb9cfdaffa7
dcdeiv <davide.dicriscito@gmail.com>
2014-07-16 01:20:13 +0200

    0.13.541
    added animations

--- Inc/main.js
+++ Inc/main.js
@@ -13,7 +13,8 @@
 		$('.soundcloud_audio_player, .audio-art').fixHeight();
 		$( '#sidebar' ).simpleLeftSidebar({
 			settings: {
-				opener: '#sb-opener'
+				opener: '#sb-opener',
+				ignore: '#ga_target, #tumblr_controls'
 			},
 			leftSidebar: {
 				style: {
@@ -21,5 +22,33 @@
 				}
 			}
 		});
+		
+		$.fn.showHeader = function() {
+			var headerTool = this,
+				mainHeader = $('#main-header');
+			$(window).resize(function() {
+				headerTool.hide();
+				$(document).scroll(function() {
+					var docTop     = $(this).scrollTop(),
+						mhTop      = mainHeader.offset().top,
+						mhHeight   = mainHeader.height(),
+						mhBottom   = mhTop + mhHeight;
+						mhPosition = mhBottom - docTop;
+						
+						console.log( mhPosition );
+						
+						if ( mhPosition <= 88 ) {
+							headerTool.show();
+						} else {
+							headerTool.hide();
+						}
+				});
+				$(document).scroll();
+			});
+			$(window).resize();
+			
+			return this;
+		};
+		$('.header-toolbar').showHeader();
 	});
 })(jQuery);
